8a1a63c539b1bb6d91f98045896642f81dc6fd27
DAMASCENO <luambito93@gmail.com>
2023-05-10 08:30:29 -0300

    JavaScript

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
-
+ 
 menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
