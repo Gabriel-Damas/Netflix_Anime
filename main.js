03ba529a28c73837a176d99b70c6daa107a8d36a
DAMASCENO <luambito93@gmail.com>
2023-05-11 20:48:45 -0300

    Html

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var swiper = new Swiper(".home-slider", {
   });
 
   var swiper = new Swiper(".anime-slider", {
-    slidesPerView: 5,
+    slidesPerView: 3,
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
