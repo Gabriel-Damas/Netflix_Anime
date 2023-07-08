b108be75ad0164a51125834b63eab6d6cff9aa99
DAMASCENO <luambito93@gmail.com>
2023-07-08 07:54:31 -0300

    hTml

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ menu.onclick = () =>{
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
-    autoplay: {
+    autoplay: { 
       delay: 7500,
       disableOnInteraction: false,
     },
