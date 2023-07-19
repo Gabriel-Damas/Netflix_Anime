d4628f072fdfa57fa41a5afead091023ae857a25
DAMASCENO <luambito93@gmail.com>
2023-07-19 08:02:25 -0300

    Html

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: { 
-      delay: 4500,
+      delay: 4500, 
       disableOnInteraction: false,
     },
     pagination: {
